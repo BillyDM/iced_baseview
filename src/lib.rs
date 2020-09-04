e64cf55d29742773ded1de7e488e436d8c32f74e
Billy Messenger <BillyDM@protonmail.com>
2020-09-04 08:56:04 -0500

    restart

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,3 @@
 #![forbid(unsafe_code)]
 #![forbid(rust_2018_idioms)]
 
-#[doc(no_inline)]
-pub use iced_native::*;
-
-mod executor;
-
-pub mod application;
-pub mod settings;
-
-pub use application::Application;
-pub use settings::Settings;
-
-pub use iced_graphics::Viewport;
-
-/// The settings of the plugin host
-#[derive(Debug, Copy, Clone)]
-pub struct HostSettings {
-    pub sample_rate: f64,
-    pub buffer_size: f64,
-    // whatever else is needed
-}
--- src/settings/windows.rs
+++
@@ -1,9 +0,0 @@
-#![cfg(target_os = "windows")]
-//! Platform specific settings for Windows.
-
-/// The platform specific window settings of an application.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
-pub struct PlatformSpecific {
-    /// Parent Window
-    pub parent: Option<winapi::shared::windef::HWND>,
-}
