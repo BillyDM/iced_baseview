adf250f8b2fb0c2bf2a330fd92e44fd9b0d65875
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-03-13 21:18:40 +0100

    Add missing OpenGL version for slider example

--- examples/slider.rs
+++ examples/slider.rs
@@ -12,9 +12,14 @@ fn main() {
             size: Size::new(500.0, 300.0),
             scale: WindowScalePolicy::SystemScaleFactor,
 
+            // FIXME: The current glow_glpyh version does not enable the correct extension in their
+            //        shader so this currently won't work with OpenGL <= 3.2
             #[cfg(feature = "glow")]
             #[cfg(not(feature = "wgpu"))]
-            gl_config: Some(baseview::gl::GlConfig::default()),
+            gl_config: Some(baseview::gl::GlConfig {
+                version: (3, 3),
+                ..baseview::gl::GlConfig::default()
+            }),
         },
         ignore_non_modifier_keys: false,
         flags: (),
